bb8c4bd07599618b2fd8b79a41907f393bf0e1ff
Facundet <facundotrejo2021@gmail.com>
2025-04-25 00:00:52 -0300

    termino pto 1 y 3

--- ToDo.c
+++ ToDo.c
@@ -18,19 +18,38 @@ typedef struct Nodo
 // ---- FUNCIONES ----
 Nodo *CrearListaVacia();
 Nodo *CrearNodo(int id);
-// Nodo * InsertarNodo();
+Nodo * InsertarNodo(Nodo* nodo, Nodo** start);
+void mostrarLista(Nodo* lista);
+
+
+
 
 int main()
 {
     srand(time(NULL));
-    int id = 1000;
-    Nodo *TareasPendientes = CrearListaVacia(); // creo dos listas
-    Nodo *TareasRealizadas = CrearListaVacia();
+    int id = 1000, seguirConTarea;
 
-    Nodo *tareaUno = CrearNodo(id);
+    Nodo *ListaTareasPendientes = CrearListaVacia(); // creo dos listas
+    Nodo *ListaTareasRealizadas = CrearListaVacia();
+
+
+    do
+    {
+        Nodo *tarea1 = CrearNodo(id++);
+        printf("\nQuiere ingresar una tarea nueva?\n>> SI = 1\n>> NO = 0\nRespuesta: \n");
+        scanf("%d", &seguirConTarea);
+        InsertarNodo(tarea1, &ListaTareasPendientes); // -- USO & PARA OBTENER LA DIRECCION DE MEMORIA DEL PUTNERO DOBLE -- 
+    } while (seguirConTarea == 1);
+
+    printf("\n//LISTA DE TAREAS PENDIENTES:");
+    mostrarLista(ListaTareasPendientes);
 
     return 0;
 }
+
+
+
+
 
 // ---- FUNCIONES ----
 Nodo *CrearListaVacia()
@@ -40,7 +59,7 @@ Nodo *CrearListaVacia()
 
 Nodo *CrearNodo(int id)
 {
-    // ESTA FUNCIÓN PARA CARGAR LA TAREA DEL USUARIO
+    // ESTA FUNCIÓN ES PARA CARGAR LA TAREA DEL USUARIO
     // NO OLVIDARME DE GUARDAR MEMORIA. SINO EL PROGRAMA NO SE EJECUTA
     Nodo *nuevoNodo = (Nodo *)malloc(sizeof(Nodo));
 
@@ -66,8 +85,29 @@ Nodo *CrearNodo(int id)
     return nuevoNodo;
 };
 
-//  Nodo * InsertarNodo() {}
+ Nodo * InsertarNodo(Nodo* nodo, Nodo** start) {
+    //-- USO PUNTERO DOBLE PARA NO TENER QUE USAR RETURN --
+
+    nodo->Siguiente = *start; //ACORDARME DE IGUALAR A UN PUNTERO
+    *start = nodo;
+}
+
+void mostrarLista(Nodo* lista){
+    int i = 0;
+    while (lista != NULL) //ESTO ES PARA ITERAR EN LA LISTA
+    {
+        printf("\nTarea %d", i + 1);
+        printf("\nTareaID: %d", lista->T->TareaID);
+        printf("\nDescripcion de la tarea: %s", lista->T->Descripcion);
+        printf("\nDuracion: %d minutos", lista->T->Duracion);
+        lista = lista->Siguiente; //ESTO ES ASÍ SIEMPRE
+        i++;
+    }
+    
+}
 
 /*
-    Desarrolle una interfaz de carga de tareas para solicitar tareas pendientes, en la cual se solicite descripción y duración de la misma (el id debe ser generado automáticamente por el sistema, de manera autoincremental comenzando desde el número 1000). Al cabo de cada tarea consulte al usuario si desea ingresar una nueva tarea o finalizar la carga.
+int TareaID;       // Numérico autoincremental comenzando en 1000
+    char *Descripcion; //
+    int Duracion; 
 */
