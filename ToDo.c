6973191753f4dd0ad0c28b0971c05020094d2335
Facundet <facundotrejo2021@gmail.com>
2025-04-27 03:17:54 -0300

    termino el punto 4

--- ToDo.c
+++ ToDo.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct Tarea
 {
@@ -21,14 +22,14 @@ Nodo * InsertarNodo(Nodo* nodo, Nodo** start); //EN ESTAR VA LA LISTA A DONDE VO
 void mostrarLista(Nodo* lista);
 void mostrarDatosNodoEncontrado (Nodo* lista);
 Nodo* BuscarPorID(Nodo* start, int ID); //Start representa el comienzo de la lista que quiero recorrer
+Nodo* BuscarPorDescripcion(Nodo* start, char* descripcion); //Paso un puntero de char porque en C los strings SON punteros. Un string como "perro" no es otra cosa que la dirección de memoria del primer caracter 'p'
+void buscarTarea(Nodo* ListaPendientes, Nodo* ListaRealizadas, int ComoBuscar, int buscarID, char* buscarDescripcion);
 Nodo* QuitarNodo(Nodo** start, int ID); //PARA QUITAR EL NODO DEBO UTILIZAR PUNTERO DOBLE ASÍ SEA MÁS FÁCIL
-
-
 
 int main()
 {
-    srand(time(NULL));
-    int id = 1000, seguirConTarea, buscarID, IDMover;
+    int id = 1000, seguirConTarea, buscarID, IDMover, ComoBuscar;
+    char buscarDescripcion[100];
 
     Nodo *ListaTareasPendientes = CrearListaVacia(); // creo dos listas
     Nodo *ListaTareasRealizadas = CrearListaVacia();
@@ -63,7 +64,28 @@ int main()
     }
     printf("\n//LISTA DE TAREAS REALIZADAS: ");
     mostrarLista(ListaTareasRealizadas);
+
+
+
+
+    puts("\n ============= Como desea buscar el nodo ============");
+    printf("\n>>Buscar por ID: 1\n>>Buscare por descripcion: 2");
+    fflush(stdin);
+    scanf("%d", &ComoBuscar);
+
+    if (ComoBuscar == 1)
+    {
+        printf("\nIngrese el ID de la tarea que desea buscar");
+        fflush(stdin);
+        scanf("%d", &buscarID); 
+    } else if (ComoBuscar == 2)
+    {
+        printf("\nIngrese la palabra que quiere buscar: ");
+        fflush(stdin);
+        gets(buscarDescripcion);
+    }
     
+    buscarTarea(ListaTareasPendientes, ListaTareasRealizadas, ComoBuscar, buscarID, buscarDescripcion);
 
     return 0;
 }
@@ -143,6 +165,18 @@ Nodo* BuscarPorID(Nodo* start, int ID){
     return NULL;
 }
 
+Nodo* BuscarPorDescripcion(Nodo* start, char* descripcion){
+    while (start != NULL)
+    {
+        if (strstr(start->T->Descripcion, descripcion))
+        {
+            return start;
+        }
+        start = start->Siguiente;
+    }
+    return NULL;
+}
+
 Nodo* QuitarNodo(Nodo** start, int ID){
     Nodo ** aux = start;
 
@@ -161,3 +195,37 @@ Nodo* QuitarNodo(Nodo** start, int ID){
     return NULL;     // Si no se encontró el ID, devolvemos NULL
     
 }
+
+void buscarTarea(Nodo* ListaPendientes, Nodo* ListaRealizadas, int ComoBuscar, int buscarID, char* buscarDescripcion)
+{
+    Nodo* encontradoPendientes = NULL;
+    Nodo* encontradoRealizadas = NULL;
+
+    if (ComoBuscar == 1)
+    {
+        encontradoPendientes = BuscarPorID(ListaPendientes, buscarID);// el resultado es el inicio del puntero 
+        encontradoRealizadas = BuscarPorID(ListaRealizadas, buscarID);
+    }
+    else if (ComoBuscar == 2)
+    {
+        encontradoPendientes = BuscarPorDescripcion(ListaPendientes, buscarDescripcion);// el resultado es el inicio del puntero 
+        encontradoRealizadas = BuscarPorDescripcion(ListaRealizadas, buscarDescripcion);
+    }
+
+    if (encontradoPendientes != NULL)
+    {
+        printf("\nTarea encontrada en pendientes:\n");
+        printf("TareaID: %d\nDescripcion: %s\nDuracion: %d minutos\n", encontradoPendientes->T->TareaID, encontradoPendientes->T->Descripcion, encontradoPendientes->T->Duracion);
+    }
+    else if (encontradoRealizadas != NULL)
+    {
+        printf("\nTarea encontrada en realizadas:\n");
+        printf("TareaID: %d\nDescripcion: %s\nDuracion: %d minutos\n", encontradoRealizadas->T->TareaID, encontradoRealizadas->T->Descripcion, encontradoRealizadas->T->Duracion);
+    }
+    else
+    {
+        printf("\nNo se encontró la tarea con el ID o palabra clave proporcionada.\n");
+    }
+}
+
+
